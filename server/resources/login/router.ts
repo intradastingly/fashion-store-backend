5a08ab519c89c9b8f335fcaf2c7c3f06f17c7c83
Intradastingly <asaf_lackgren@hotmail.com>
2021-05-19 13:44:25 +0200

    order schema and loginsession

---
+++ server/resources/login/router.ts
@@ -0,0 +1,11 @@
+export {};
+const express = require('express');
+const loginRouter = express.Router();
+const controller = require("./controller");
+
+loginRouter
+    .post("/login", controller.login)
+    .delete("/logout", controller.logout)
+    .get("/authenticated", controller.authenticate)
+
+module.exports = loginRouter
