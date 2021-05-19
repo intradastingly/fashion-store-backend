f266aafe861aff3d5f2d82c5ca4e43dd6f737ffb
supertramps <alexander.liljedahl@live.se>
2021-05-19 13:55:49 +0200

    Started working on profile page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,11 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { CSSProperties } from "react";
 import "./App.css";
 import AdminEditDetails from "./componenets/Admin/AdminEditDetails";
 import AdminList from "./componenets/Admin/AdminList";
 import AdminLogIn from "./componenets/Admin/AdminLogIn";
+import userLogIn from "./componenets/User/logIn";
+import UserProfile from "./componenets/User/profile";
 import CartView from "./componenets/Cart/CartView";
 import Footer2 from "./componenets/Footer";
 import Navbar from "./componenets/Navbar";
@@ -18,21 +21,34 @@ function App() {
     <CartProvider>
       <Router>
         <ScrollToTop />
-        <Navbar />
-        <Switch>
-          <Route path="/product/:id" component={ProductDetails} />
-          <Route path="/ordersuccess" component={OrderSuccessMessage} />
-          <Route exact path="/" component={StartPageView} />
-          <Route path="/cart" component={CartView} />
-          <Route path="/admin" component={AdminLogIn} />
-          <Route path="/admin-list" component={AdminList} />
-          <Route path="/add-product" component={AddNewProduct} />
-          <Route path="/edit-product/:id" component={AdminEditDetails} />
-        </Switch>
-        <Footer2 />
+        <div style={appContainer}>
+          <div>
+            <Navbar />
+          </div>
+          <div>
+            <Switch>
+              <Route path="/product/:id" component={ProductDetails} />
+              <Route path="/ordersuccess" component={OrderSuccessMessage} />
+              <Route exact path="/" component={StartPageView} />
+              <Route path="/cart" component={CartView} />
+              <Route path="/login" component={userLogIn} />
+              <Route path="/profile" component={UserProfile} />
+              <Route path="/admin" component={AdminLogIn} />
+              <Route path="/admin-list" component={AdminList} />
+              <Route path="/add-product" component={AddNewProduct} />
+              <Route path="/edit-product/:id" component={AdminEditDetails} />
+            </Switch>
+          </div>
+          <Footer2 />
+        </div>
       </Router>
     </CartProvider>
   );
 }
+
+const appContainer: CSSProperties = {
+  display: "flex",
+  flexDirection: "column",
+};
 
 export default App;
