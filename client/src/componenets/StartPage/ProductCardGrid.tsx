4d272a66109de6b00a04f402977aa0ddc3f3687a
Intradastingly <asaf_lackgren@hotmail.com>
2021-05-25 12:01:46 +0200

    works to sort products based on category

--- client/src/componenets/StartPage/ProductCardGrid.tsx
+++ client/src/componenets/StartPage/ProductCardGrid.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, CSSProperties, useState } from 'react';
+import React, { useContext, CSSProperties, useState, useEffect } from 'react';
 import { Card, Col, List, Row, message } from 'antd';
 import { ShoppingCartOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
@@ -16,67 +16,96 @@ const tagsData = ["All","Dresses", "Jeans", "Coats", "Blazers", "T-shirts", "Jum
 function ProductCardGrid(){
     const {addProductToCart} = useContext(CartContext);
     const {allProducts, getOrder, loggedIn} = useContext(ApiContext);
-    const [selectedTags, setSelectedTags] = useState<any>("All")
-    console.log(allProducts)
-    console.log(selectedTags)
+    const [selectedTags, setSelectedTags] = useState<any>(["All"]);
+    const [filteredCategories, setFilteredCategories] = useState<any>();
+    
+    console.log(filteredCategories)
+
+    function filterArray(array: any[], filters: any) {
+        const filterKeys = Object.keys(filters);
+        return array.filter(item => {
+          // validates all filter criteria
+          return filterKeys.every(key => {
+            // ignores non-function predicates
+            if (typeof filters[key] !== 'function') return true;
+            return filters[key](item[key]);
+          });
+        });
+      }
+
+    useEffect(()=>{
+        if(allProducts){
+            const filter = {
+                category: (category: any) => {
+                    for(const c of selectedTags){
+                        if(category.includes(c)) return true;
+                    }
+                }
+            }
+            const filteredObject = filterArray(allProducts, filter)
+            setFilteredCategories(filteredObject);
+        }
+    }, [selectedTags])
    
-   /*  allProducts.category.map((c:any) => {
-        console.log(c)
-    }) */
+  
 
     function handleChange(tag: any,checked: any){
         const nextSelectedTags = checked ? [...selectedTags, tag] : selectedTags.filter((t: any) => t !== tag);
         setSelectedTags(nextSelectedTags)
     }
 
-        return(
-            <Row style={cardContainer}>
-                <Row style={categoriesContainer}>
-                    {tagsData.map(tag => (
-                    <CheckableTag
-                        key={tag}
-                        style={tagStyle}
-                        checked={selectedTags.indexOf(tag) > -1}
-                        onChange={checked => handleChange(tag, checked)}
-                    >
-                        {tag}
-                    </CheckableTag>
-                    ))}
-                </Row>
-                <Col span={24} style={columnStyle}>
-                    <List
-                       grid={{
-                            gutter: 25,
-                            xs: 1,
-                            sm: 2,
-                            md: 2,
-                            lg: 4,
-                            xl: 4,
-                            xxl: 4,
-                        }}
-                        dataSource={allProducts}
-                        renderItem={item => (
-                            <List.Item >
-                                <Link to={'/product/' + item._id}>
-                                    <Card
-                                        hoverable
-                                        cover={<img src={item.img} alt='product' />}
-                                        actions={[
-                                            <ShoppingCartOutlined 
-                                                style={{ fontSize: '2rem' }}
-                                                onClick={(e) => {success(); e.preventDefault(); addProductToCart(item, undefined)}} 
-                                            />
-                                        ]}
-                                    >
-                                    <Meta title={item.title} description={item.price + "kr"} />
-                                    </Card>
-                                </Link>
-                            </List.Item>
-                        )} 
-                    />
-                </Col>
+    return(
+        <Row style={cardContainer}>
+            <Row style={categoriesContainer}>
+                {tagsData.map(tag => (
+                <CheckableTag
+                    key={tag}
+                    style={tagStyle}
+                    checked={selectedTags.indexOf(tag) > -1}
+                    onChange={checked => handleChange(tag, checked)}
+                >
+                    {tag}
+                </CheckableTag>
+                ))}
             </Row>
-        )
+            <Col span={24} style={columnStyle}>
+                {
+                <List
+                    grid={{
+                        gutter: 25,
+                        xs: 1,
+                        sm: 2,
+                        md: 2,
+                        lg: 4,
+                        xl: 4,
+                        xxl: 4,
+                    }}
+                    dataSource={filteredCategories}
+                    renderItem={(item: any) => (
+                        <List.Item >
+                            <Link to={'/product/' + item._id}>
+                                <Card
+                                    hoverable
+                                    cover={<img src={item.img} alt='product' />}
+                                    actions={[
+                                        <ShoppingCartOutlined 
+                                            style={{ fontSize: '2rem' }}
+                                            onClick={(e) => {success(); e.preventDefault(); addProductToCart(item, undefined)}} 
+                                        />
+                                    ]}
+                                >
+                                <Meta title={item.title} description={item.price + "kr"} />
+                                </Card>
+                                
+                            </Link>
+                            
+                        </List.Item>
+                    )} 
+                />
+                }
+            </Col>
+        </Row>
+    )
    
 }
 
@@ -97,7 +126,7 @@ const categoriesContainer: CSSProperties = {
     alignItems: 'space-around',
     width: '100%',
     margin: 'auto',
-    marginTop: '1rem',
+    paddingTop: '1.1rem',
     marginBottom: '0.5rem',
 }
 
