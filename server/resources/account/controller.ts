de76c0514f5c56af664b8872ff52cf239129daf3
Sebastian Zazzi <sebastian@zazzi.se>
2021-05-25 09:47:02 +0200

    model for accounts now complete

--- server/resources/account/controller.ts
+++ server/resources/account/controller.ts
@@ -1,18 +1,18 @@
 export { };
-import { profile } from "console";
-import express from "express";
+import { Request, Response} from "express";
 const Account = require("./model");
 
 
 // Create new
-exports.newAccount = async (req: express.Request, res: express.Response) => {
+exports.newAccount = async (req: Request, res: Response) => {
   const account = new Account({
     userName: req.body.userName,
     fullName: req.body.fullName,
     role: "plebian",
     password: req.body.password,
     email: req.body.email,
-    address: req.body.address
+    address: req.body.address,
+    phoneNumber: req.body.phoneNumber
   });
 
   await account.save();
@@ -21,18 +21,19 @@ exports.newAccount = async (req: express.Request, res: express.Response) => {
 
 // Get all
 exports.getAllAccounts = async (
-  req: express.Request,
-  res: express.Response
+  req: Request,
+  res: Response
 ) => {
   const account = await Account.find();
 
   res.status(200).json(account);
 };
 
+
 // get specific 
 exports.getSpecificAccount = async (
-  req: express.Request,
-  res: express.Response
+  req: Request,
+  res: Response
 ) => {
   // Långa id crashar server? problem eller okej?
   const specificAccount = await Account.findOne({ _id: req.params.id });
@@ -45,14 +46,14 @@ exports.getSpecificAccount = async (
 
 
 // Delete
-exports.deleteAccount = async (req: express.Request, res: express.Response) => {
+exports.deleteAccount = async (req: Request, res: Response) => {
   const deletedAccount = await Account.findOneAndDelete({ _id: req.params.id });
   res.status(200).json(deletedAccount);
 
 };
 
 // Edit
-exports.editAccount = async (req: express.Request, res: express.Response) => {
+exports.editAccount = async (req: Request, res: Response) => {
   const account = await Account.findOneAndUpdate(
     { _id: req.params.id },
     {
--- server/resources/account/model.ts
+++ server/resources/account/model.ts
@@ -6,29 +6,32 @@ const saltRounds = 10;
 interface Address {
   street: String, 
   zipCode: Number,
-  city: String
+  city: String,
+  country: String
 }
 export interface AccountDocument extends MongooseDocument {
   userName: String,
   fullName: String,
+  phoneNumber: String,
   role: String,
   password: String,
   email: String;
-  address: Address;
+  address: Address,
 }
 
 const accountSchema = new Schema<AccountDocument>({
   userName: { type: String },
   fullName: {type: String},
+  phoneNumber: { type: String},
   role: { type: String },
   password: { type: String, select: false },
   email: {type: String},
-  address: { type: Object}
+  address: { type: Object},
 });
 
+// Hashing password middleware
 accountSchema.pre('save', async function (next) {
   const account = this;
-
   if (this.isModified("password") || this.isNew) {
     const hashedPassword = await bcrypt.hash(account.password, saltRounds)
     account.password = hashedPassword;
