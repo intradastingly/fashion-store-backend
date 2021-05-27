2bc15748672633453d4a681df82e7404da0f3877
Intradastingly <asaf_lackgren@hotmail.com>
2021-05-27 11:10:59 +0200

    order data displays properly on checkout

--- client/src/componenets/OrderSuccess/OrderSuccessMessage.tsx
+++ client/src/componenets/OrderSuccess/OrderSuccessMessage.tsx
@@ -7,6 +7,24 @@ import { ApiContext } from "../../contexts/ApiContext";
 function OrderSuccessMessage() {
     const {order} = useContext(ApiContext)
 
+    if(!order){
+        return (
+            <Row style={containerStyle}>
+            <Col span={24} style={colStyle}>
+                <Result
+                    status="warning"
+                    title="No Order to display"
+                    extra={[
+                    <Link to='/'>
+                        <Button type="primary" key="console">Continue shopping</Button>
+                    </Link>
+                    ]}
+                />
+            </Col>
+        </Row>
+        )
+    }
+
      return (
         <Row style={containerStyle}>
             <Col span={24} style={colStyle}>
