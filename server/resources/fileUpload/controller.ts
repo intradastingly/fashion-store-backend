ee9779acdb5feadb571c7bb5a228980884220fa0
Intradastingly <asaf_lackgren@hotmail.com>
2021-05-28 10:52:47 +0200

    hhmmhmhmhmh

--- server/resources/fileUpload/controller.ts
+++ server/resources/fileUpload/controller.ts
@@ -3,7 +3,7 @@ const File = require("../fileUpload/model")
 
 exports.upload = (req: any, res: express.Response) => {
     console.log(req.files)
-
+    res.status(200).send()
     /* if(req.files?.image) {
         const fileName = Date.now() + '-' + req.files.image.name;
         req.files.image.mv(`uploads/${fileName}`, () => {
