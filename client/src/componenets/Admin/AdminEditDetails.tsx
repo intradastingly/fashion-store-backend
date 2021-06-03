cbb15f6d9730562a87bd637dc65889c7c71bb657
Intradastingly <asaf_lackgren@hotmail.com>
2021-06-03 10:05:35 +0200

    yuh

--- client/src/componenets/Admin/AdminEditDetails.tsx
+++ client/src/componenets/Admin/AdminEditDetails.tsx
@@ -39,7 +39,7 @@ function AdminEditDetails(props: Props, state: State) {
   const [priceField, setPriceField] = useState(editProduct.price);
   const [imageField, setImageField] = useState(editProduct.image);
   const [quantityField, setQuantityField] = useState(editProduct.quantity);
-  const [upload, setUpload] = useState<any>()
+  const [error, setErrorMessage] = useState<string>();
 
   const options = [
     { value: "All" },
@@ -167,7 +167,13 @@ function AdminEditDetails(props: Props, state: State) {
       }
     })
     const imgPath = await response.json();
-    saveProduct(imgPath)
+    if(imgPath === "Invalid file type"){
+      setErrorMessage(imgPath)
+      return false;
+    } else {
+      deleteOldImage(editProduct.img)
+      saveProduct(imgPath)
+    } 
   }
 
   const deleteOldImage = async (img: string) => {  
@@ -240,7 +246,6 @@ function AdminEditDetails(props: Props, state: State) {
           type="primary"
           onClick={() => {
               saveNewImage()
-              deleteOldImage(editProduct.img)
             }
           }
         >
